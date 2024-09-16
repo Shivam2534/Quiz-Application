ea5dc88454cbd8ea6227d1fe12c9233dc72d8c60
shivam2534 <134926520+shivamkanchole@users.noreply.github.com>
2024-09-16 20:18:51 +0530

    add timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { quizData as q } from "./data.js";
 import { useEffect } from "react";
+import Timer from "./Timer.jsx";
 
 function App() {
   const [curr, setcurr] = useState(0);
@@ -93,99 +94,107 @@ function App() {
   }, 3000);
 
   return curr < q.length ? (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white shadow-lg rounded-lg p-8 max-w-xl w-full">
-        <div className="mb-6">
-          <h2 className="text-xl font-semibold text-gray-800 mb-4">
-            {curr + 1}. {q[curr].question}
-          </h2>
+    <div>
+      <p className="border border-blue-200 justify-start flex pl-2 sm:pl-10 h-10 items-center">
+        Hello👋, <b>shivam</b>
+      </p>
+      <p className=" justify-center sm:justify-end flex sm:pr-10 h-10 items-center bg-blue-100 ">
+        <Timer setcurr={setcurr}/>
+      </p>
+      <div className="px-2 min-h-screen bg-gray-100 flex items-center justify-center">
+        <div className="bg-white shadow-lg rounded-lg p-8 max-w-xl w-full mb-24">
+          <div className="mb-6">
+            <h2 className="text-xl font-semibold text-gray-800 mb-4">
+              {curr + 1}. {q[curr].question}
+            </h2>
 
-          <div
-            className="flex items-center mb-4 p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100"
-            onClick={() => setuserAnswer("a")}
-          >
-            <input
-              type="radio"
-              name="checkAnswer"
-              id="a"
-              value="a"
-              checked={userAnswer === "a"} // Set checked based on userAnswer state
-              onChange={() => setuserAnswer("a")}
-              className="mr-4"
-            />
-            <p className="text-gray-700">{q[curr].a}</p>
-          </div>
-
-          <div
-            className="flex items-center mb-4 p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100"
-            onClick={() => setuserAnswer("b")}
-          >
-            <input
-              type="radio"
-              name="checkAnswer"
-              id="b"
-              value="b"
-              checked={userAnswer === "b"}
-              onChange={() => setuserAnswer("b")}
-              className="mr-4"
-            />
-            <p className="text-gray-700">{q[curr].b}</p>
-          </div>
-
-          <div
-            className="flex items-center mb-4 p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100"
-            onClick={() => setuserAnswer("c")}
-          >
-            <input
-              type="radio"
-              name="checkAnswer"
-              id="c"
-              value="c"
-              checked={userAnswer === "c"}
-              onChange={() => setuserAnswer("c")}
-              className="mr-4"
-            />
-            <p className="text-gray-700">{q[curr].c}</p>
-          </div>
-
-          <div
-            className="flex items-center mb-4 p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100"
-            onClick={() => setuserAnswer("d")}
-          >
-            <input
-              type="radio"
-              name="checkAnswer"
-              id="d"
-              value="d"
-              checked={userAnswer === "d"}
-              onChange={() => setuserAnswer("d")}
-              className="mr-4"
-            />
-            <p className="text-gray-700">{q[curr].d}</p>
-          </div>
-
-          <div className="flex items-center justify-between mt-6">
-            <div>
-              {erroMsg && (
-                <p className="text-red-500 text-sm mb-0 ">{erroMsg}</p>
-              )}
+            <div
+              className="flex items-center mb-4 p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100"
+              onClick={() => setuserAnswer("a")}
+            >
+              <input
+                type="radio"
+                name="checkAnswer"
+                id="a"
+                value="a"
+                checked={userAnswer === "a"} // Set checked based on userAnswer state
+                onChange={() => setuserAnswer("a")}
+                className="mr-4"
+              />
+              <p className="text-gray-700">{q[curr].a}</p>
             </div>
 
-            <div className="flex gap-4 w-1/2 justify-end">
-              <button
-                type="button"
-                onClick={() => PrevQuetions(q[curr - 1].id, curr - 1)}
-                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200"
-              >
-                Prev
-              </button>
-              <button
-                type="button"
-                onClick={() => AddUserTickedAnswer(curr)}
-                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200"
-              >
-                Next
-              </button>
+            <div
+              className="flex items-center mb-4 p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100"
+              onClick={() => setuserAnswer("b")}
+            >
+              <input
+                type="radio"
+                name="checkAnswer"
+                id="b"
+                value="b"
+                checked={userAnswer === "b"}
+                onChange={() => setuserAnswer("b")}
+                className="mr-4"
+              />
+              <p className="text-gray-700">{q[curr].b}</p>
+            </div>
+
+            <div
+              className="flex items-center mb-4 p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100"
+              onClick={() => setuserAnswer("c")}
+            >
+              <input
+                type="radio"
+                name="checkAnswer"
+                id="c"
+                value="c"
+                checked={userAnswer === "c"}
+                onChange={() => setuserAnswer("c")}
+                className="mr-4"
+              />
+              <p className="text-gray-700">{q[curr].c}</p>
+            </div>
+
+            <div
+              className="flex items-center mb-4 p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100"
+              onClick={() => setuserAnswer("d")}
+            >
+              <input
+                type="radio"
+                name="checkAnswer"
+                id="d"
+                value="d"
+                checked={userAnswer === "d"}
+                onChange={() => setuserAnswer("d")}
+                className="mr-4"
+              />
+              <p className="text-gray-700">{q[curr].d}</p>
+            </div>
+
+            <div className="flex items-center justify-between mt-6">
+              <div>
+                {erroMsg && (
+                  <p className="text-red-500 text-sm mb-0 ">{erroMsg}</p>
+                )}
+              </div>
+
+              <div className="flex gap-4 w-1/2 justify-end">
+                <button
+                  type="button"
+                  onClick={() => PrevQuetions(q[curr - 1].id, curr - 1)}
+                  className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200"
+                >
+                  Prev
+                </button>
+                <button
+                  type="button"
+                  onClick={() => AddUserTickedAnswer(curr)}
+                  className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200"
+                >
+                  Next
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -193,22 +202,21 @@ function App() {
     </div>
   ) : (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-  <div className="bg-white shadow-md rounded-lg p-6 text-center">
-    <h1 className="text-2xl font-bold text-gray-800 mb-4">
-      You have Scored {Score}/{q.length}
-    </h1>
-    <p className="text-lg text-gray-600 mb-4">
-      Refresh the page to retake the quiz!
-    </p>
-    <button
-      onClick={() => window.location.reload()}
-      className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-200"
-    >
-      Retake Quiz
-    </button>
-  </div>
-</div>
-
+      <div className="bg-white shadow-md rounded-lg p-6 text-center">
+        <h1 className="text-2xl font-bold text-gray-800 mb-4">
+          You have Scored {Score}/{q.length}
+        </h1>
+        <p className="text-lg text-gray-600 mb-4">
+          Refresh the page to retake the quiz!
+        </p>
+        <button
+          onClick={() => window.location.reload()}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-200"
+        >
+          Retake Quiz
+        </button>
+      </div>
+    </div>
   );
 }
 
---
+++ src/Timer.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { quizData as q } from "./data.js";
+
+function Timer({ setcurr }) {
+  const duration = 3*60; // sec
+  const [timeLeft, setTimeLeft] = useState(duration);
+
+  useEffect(() => {
+    if (timeLeft <= 0) {
+      console.log("publish results");
+      setcurr(q.length);
+    }
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [timeLeft]);
+
+  // Format timeLeft in hh:mm:ss format
+  const formatTime = (seconds) => {
+    let dateObj = new Date(seconds * 1000);
+    let hour = dateObj.getUTCHours();
+    let minute = dateObj.getUTCMinutes();
+    let second = dateObj.getSeconds();
+
+    return `${hour}:${minute < 10 ? "0" : ""}${minute}:${
+      second < 10 ? "0" : ""
+    }${second}`;
+  };
+
+  return (
+    <div className=" border border-gray-500 text-center bg-gray-200 text-gray-700 py-2 px-4 rounded-lg">
+      <p className="font-semibold ">Time Remaining: {formatTime(timeLeft)}</p>
+    </div>
+  );
+}
+
+export default Timer;
